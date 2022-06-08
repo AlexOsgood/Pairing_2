03e5e14c4a3917288fe03164e002a81c26dd2e5e
Alex Osgood <alex.osgood@gmail.com>
2022-06-08 10:47:11 -0600

    Added places

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Alex", "Colin", "Matt", "Amy", "Kate"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+var places = ["Denver", "SC", "CO", "NC", "KS"];
+places.pop();
+console.log(places);
